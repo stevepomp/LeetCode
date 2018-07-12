090e0f37f9b97335741189af1879c466efc4d5b2
python-DIY <raoyunxiao@gmail.com>
2018-07-12 15:37:04 +0800

    Create TwoSum.py

---
+++ python/TwoSum.py
@@ -0,0 +1,20 @@
+'''
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+'''
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        d = {}
+        for i, element in enumerate(nums):
+            if target-element in d:
+                return [d[target-element], i]
+            d[element] = i
