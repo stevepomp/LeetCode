31f15f81a1ac23872989d9309610e0db3ec58252
python-DIY <raoyunxiao@gmail.com>
2018-07-26 21:21:45 +0800

    Update generate.py

--- python/118. Pascal-s Triangle/generate.py
+++ python/118. Pascal-s Triangle/generate.py
@@ -17,3 +17,21 @@ Output:
 '''
 
 
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        result = []
+        if numRows == 1:
+            return [[1]]
+        if numRows == 2:
+            return [[1], [1, 1]]
+        if numRows >= 3:
+            result = [[1] * i for i in range(1, numRows+1)]
+            for i in range(2, numRows):
+                for j in range(1, i):
+                    result[i][j] = result[i-1][j-1] + result[i-1][j]
+        return result
+                
