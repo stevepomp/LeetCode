8f96f50d6d5e1e6a451a7553e36eb77da01f4827
python-DIY <raoyunxiao@gmail.com>
2018-07-26 20:45:35 +0800

    Create search.py

---
+++ python/033. Search in Rotated Sorted Array/search.py
@@ -0,0 +1,20 @@
+'''
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+'''
+
+
+class Solution:
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if target in nums:
+            return nums.index(target)
+        else:
+            return -1
