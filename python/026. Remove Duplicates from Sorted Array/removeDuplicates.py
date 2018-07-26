0d1763433583eb3f7f7f1d4177104f67efea92ee
python-DIY <raoyunxiao@gmail.com>
2018-07-26 20:40:10 +0800

    Create removeDuplicates.py

---
+++ python/026. Remove Duplicates from Sorted Array/removeDuplicates.py
@@ -0,0 +1,27 @@
+'''
+Given nums = [1,1,2],
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+It doesn't matter what you leave beyond the returned length.
+
+Given nums = [0,0,1,1,1,2,2,3,3,4],
+Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
+It doesn't matter what values are set beyond the returned length.
+'''
+
+
+class Solution:
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        lo = 0
+        for i in range(1, len(nums)):
+            if nums[i] != nums[lo]:
+                lo += 1
+                nums[lo] = nums[i]
+        nums = nums[:lo+1]
+        return lo+1
+            
