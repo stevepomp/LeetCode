438831a025a03545eb06af53e55a2a4955a4cbf6
python-DIY <raoyunxiao@gmail.com>
2018-07-26 20:35:53 +0800

    Update removeNthFromEnd.py

--- python/019. Remove Nth Node From End of List/removeNthFromEnd.py
+++ python/019. Remove Nth Node From End of List/removeNthFromEnd.py
@@ -4,6 +4,11 @@ After removing the second node from the end, the linked list becomes 1->2->3->5.
 '''
 
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 class Solution:
     def removeNthFromEnd(self, head, n):
         """
